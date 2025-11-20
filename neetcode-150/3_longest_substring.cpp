2a20493df7c9d0391110035b73a0e1545119c20d
turtle-geek <ethanyzzhang969@gmail.com>
2025-11-20 11:47:45 -0500

    optimize sliding window logic

--- neetcode-150/3_longest_substring.cpp
+++ neetcode-150/3_longest_substring.cpp
@@ -10,34 +10,20 @@ public:
          */
 
         std::unordered_set<int> seen_char;
-        int s_pos = 0;
-        int t = 0;
+        int l = 0;
+        int r = 0;
         int max_len = 0;
-        int curr_len = 0;
 
-        while(t < s.length()){
-            char curr_char = s[t];
-            if(!seen_char.contains(curr_char)){
-                seen_char.insert(curr_char);
-                curr_len++;
-            } else {
-                if(curr_len > max_len){
-                    max_len = curr_len;
-                }
-                while(s[s_pos] != curr_char){
-                    seen_char.erase(s[s_pos]);
-                    s_pos++;
-                    curr_len--; // off by one
-                }
-                seen_char.erase(s[s_pos]);
-                s_pos++;
-
-                seen_char.insert(curr_char);
+        for(int r = 0; r < s.length(); r++){
+            while(seen_char.contains(s[r])){
+                seen_char.erase(s[l]);
+                l++;
             }
-            t++;
-        }
-        if(curr_len > max_len){
-            max_len = curr_len;
+            seen_char.insert(s[r]);
+            int curr_len = r - l + 1;
+            if(curr_len > max_len){
+                max_len = curr_len;
+            }
         }
         return max_len;
     }
