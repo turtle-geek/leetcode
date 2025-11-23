33d9cf3c510f28eed7d079438130484e174fb84b
turtle-geek <ethanyzzhang969@gmail.com>
2025-11-22 23:51:32 -0500

    buy and sell stock with sliding window algorithm

---
+++ neetcode-150/121_best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        /**
+         * Time complexity: O(n) for itera
+         * Space complexity: O(1) for using constant # of variables
+         */
+        int l = 0;
+        int r = 0;
+        int profit = 0;
+        for(r = 0; r < prices.size(); r++){
+            while(prices[l] > prices[r]){
+                l++;
+            }
+            profit = std::max(profit, prices[r] - prices[l]);
+        }
+        return profit;
+    }
+};
