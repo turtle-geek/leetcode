851c14e0694ea78a947db779726934f853a613e3
turtle-geek <ethanyzzhang969@gmail.com>
2025-11-23 21:50:22 -0500

    valid palindrome with string cleaning

---
+++ neetcode-150/125_valid_palindrome.cpp
@@ -0,0 +1,30 @@
+#include <string>
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        /**
+         * Time Complexity: O(n) for iterating through string
+         */
+
+        std::string clean_string = "";
+        
+        for(char ch : s){
+            if(std::isalnum(static_cast<unsigned char>(ch))){
+                clean_string += static_cast<char>(
+                    std::tolower(static_cast<unsigned char>(ch)));
+            }
+        }
+
+        int l = 0;
+        int r = clean_string.length() - 1;
+        while(l < r){
+            if(clean_string[l] != clean_string[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
