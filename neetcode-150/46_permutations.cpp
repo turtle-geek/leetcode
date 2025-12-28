77b005aa21b7ec8e5612f97926a849c4cfef7a76
turtle-geek <ethanyzzhang969@gmail.com>
2025-12-28 12:32:52 -0500

    permutations

---
+++ neetcode-150/46_permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void backtrack(int start, vector<int>& nums, vector<vector<int>>& result) {
+        if (start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); i++) {
+            swap(nums[start], nums[i]);
+            backtrack(start + 1, nums, result);
+            swap(nums[start], nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        backtrack(0, nums, result);
+        return result;
+    }
+};
