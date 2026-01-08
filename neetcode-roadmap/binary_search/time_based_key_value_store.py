6fb20eb65b2a3600d9b81cc8243787c00ce09824
turtle-geek <ethanyzzhang969@gmail.com>
2026-01-08 16:59:44 -0500

    time based key value store

---
+++ neetcode-roadmap/binary_search/time_based_key_value_store.py
@@ -0,0 +1,40 @@
+class TimeMap(object):
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key, value, timestamp):
+        """
+        :type key: str
+        :type value: str
+        :type timestamp: int
+        :rtype: None
+        """
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([timestamp, value])
+        
+
+    def get(self, key, timestamp):
+        """
+        :type key: str
+        :type timestamp: int
+        :rtype: str
+        """
+        if key not in self.store:
+            return ""
+        
+        values = self.store[key]
+        
+        idx = bisect.bisect_right(values, [timestamp, chr(127)])
+        
+        if idx == 0:
+            return ""
+        
+        return values[idx - 1][1]
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
