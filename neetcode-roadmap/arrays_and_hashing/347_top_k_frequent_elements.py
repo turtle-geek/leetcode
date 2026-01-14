2fe9e3131683fb375df7326098626989bf2a8412
turtle-geek <ethanyzzhang969@gmail.com>
2026-01-14 18:48:31 -0500

    review top k frequent elements

--- neetcode-roadmap/arrays_and_hashing/347_top_k_frequent_elements.py
+++ neetcode-roadmap/arrays_and_hashing/347_top_k_frequent_elements.py
@@ -7,6 +7,13 @@ class Solution(object):
         """
         # Time Complexity: O(n) where n is number of items in buckets
         # Space Complexity: O(n) total number of items stored in the buckets
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
         count = {}
         freq = [[] for i in range(len(nums)+1)]
 
@@ -15,9 +22,9 @@ class Solution(object):
         for n, c in count.items():
             freq[c].append(n)
         
-        res = []
+        k_freq = []
         for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+                k_freq.append(n)
+                if len(k_freq) == k:
+                    return k_freq
