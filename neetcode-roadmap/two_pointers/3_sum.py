f86f81a509680432b08395cec51c55e4fcf0e414
turtle-geek <ethanyzzhang969@gmail.com>
2026-01-25 16:49:45 -0500

    update 3sum

--- neetcode-roadmap/two_pointers/3_sum.py
+++ neetcode-roadmap/two_pointers/3_sum.py
@@ -4,35 +4,40 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # Time Complexity: O(n^2)
+        # Time Complexity: O(n^2) for iterating through the unique list values and secondary pointers
         # Space Complexity: O(n) for result list
         res = []
         nums.sort()
-        
+
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            if i > 0 and nums[i] == nums[i-1]: continue
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             
             l = i + 1
             r = len(nums) - 1
-            
+
             while l < r:
-                three_sum = nums[i] + nums[l] + nums[r]
+                total = nums[i] + nums[l] + nums[r]
 
-                if three_sum > 0:
+                if total > 0:
                     r -= 1
-                elif three_sum < 0:
-                    l += 1
-                else:
-                    res.append([nums[i], nums[l], nums[r]])
 
-                    while l < r and nums[l] == nums[l + 1]:
+                elif total < 0:
+                    l += 1
+                
+                else:
+                    # total = 0
+                    # -1,-1, 1,1,1,2,2
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == [l + 1]:
                         l += 1
                     while l < r and nums[r] == nums[r - 1]:
                         r -= 1
-                    
+
                     l += 1
                     r -= 1
-                
+
         return res
