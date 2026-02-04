6a8b3f910809055b515d004d12226081886b717a
turtle-geek <ethanyzzhang969@gmail.com>
2026-02-04 17:38:14 -0500

    minimum stack

---
+++ neetcode-roadmap/stacks/minimum_stack.py
@@ -0,0 +1,26 @@
+class MinStack:
+    # Time Complexity: O(1) for all operations
+    # Space Compleixty: O(n) where n is the number of elements in the stack
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or self.min_stack[-1] >= val:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if not self.stack:
+            return
+        
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+            
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
