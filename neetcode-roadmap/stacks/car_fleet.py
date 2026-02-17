3a321f07b2e7d83fdc96984b8eebf143e0b7c788
turtle-geek <ethanyzzhang969@gmail.com>
2026-02-16 19:19:50 -0500

    update car fleet

--- neetcode-roadmap/stacks/car_fleet.py
+++ neetcode-roadmap/stacks/car_fleet.py
@@ -6,8 +6,8 @@ class Solution(object):
         :type speed: List[int]
         :rtype: int
         """
-        # Time complexity: O(n) for iterating through position and speed
-        # Space complexity: O(n) for all elements in stack in worst case
+        # Time Complexity: O(nlogn) for sorting the list
+        # Space Compleixty: O(n) for list of cars
         cars = sorted(zip(position, speed), reverse=True)
         fleets = 0
         last_fleet_time = 0.0
