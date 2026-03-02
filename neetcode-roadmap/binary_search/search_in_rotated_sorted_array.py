e9ea32c6b79671de329910b63c95caf83ac077cf
turtle-geek <ethanyzzhang969@gmail.com>
2026-03-01 19:37:06 -0500

    update search in sorted array

--- neetcode-roadmap/binary_search/search_in_rotated_sorted_array.py
+++ neetcode-roadmap/binary_search/search_in_rotated_sorted_array.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         left, right = 0, len(nums) - 1
         
-        while left <= right:
+        while left <= right: 
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
